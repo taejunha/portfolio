bd114783a075fbf40e99e0a1862cfb3c992b434d
Taejun Ha <tha7@uwo.ca>
2024-07-06 18:16:27 -0400

    changed hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,29 @@
 import { motion, useAnimation } from "framer-motion";
 import { fadeIn, textVariant } from "../utils/motion";
-import Slide from "../components/Slide";
 
 const Hero = ({ scrollContainer }) => {
   return (
     <section className="parallax">
-      <div className="parallax__content absolute top-[1%] sm:top-[10%] lg:top-[9%] w-full flex flex-col lg:flex-row lg:left-[10%] md:left-[10%] sm:left-[5%]">
-        <div className="flex-1 lg:mb-0 top-5 lg:left-[40%]">
-        <motion.h1
+      <div className="parallax__content absolute top-[1%] sm:top-[15%] lg:top-[9%] w-full flex flex-col lg:flex-row lg:left-[6%] md:left-[10%] sm:left-[5%]">
+        <div className="flex-1 lg:mb-0 top-5">
+        <motion.div
             className="font-bold text-white text-[40px] sm:text-[50px] md:text-[70px] lg:text-[80px] 2xl:text-[90px] leading-[110px] 2xl:leading-[160px] "
             variants={fadeIn("down", "spring", 0.5, 1.5)}
             initial="hidden"
             animate="show"
           >
-            Taejun Ha
-        </motion.h1>
+            <h1 className="name__ha">Hi, I'm Taejun Ha</h1>
+            <div className="roller">
+              <span id="rolltext" className="sm:text-[30px] md:text-[55px] lg:text-[60px] 2xl:text-[60px]">Software developer<br/>
+                Blackhawks fan<br/>
+                Traveller<br/>
+                Runner<br/>
+              </span>
+              <br/>
+            </div>
+        </motion.div>
         </div>
       </div>
-      <Slide/>
       <img className="parallax_front" src="./parallax/ft.svg" alt="" />
       <img className="parallax_hill" src="./parallax/mt.svg" alt="" />
       <img className="parallax_sky" src="./parallax/skyline.svg" alt="" />
