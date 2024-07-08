97b9989e6dc6fa51fc670ebbb135d9600f357181
Taejun Ha <tha7@uwo.ca>
2024-07-08 16:14:30 -0400

    styling changes to title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,30 @@
-import { motion, useAnimation } from "framer-motion";
-import { fadeIn, textVariant } from "../utils/motion";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 const Hero = ({ scrollContainer }) => {
   return (
     <section className="parallax">
-      <div className="parallax__content absolute top-[1%] sm:top-[15%] lg:top-[9%] w-full flex flex-col lg:flex-row lg:left-[6%] md:left-[10%] sm:left-[5%]">
+      <div className="parallax__content absolute top-[1%] sm:top-[15%] lg:top-[9%] w-full flex flex-col items-center justify-center sm:right-[5%] md:right-[5%] lg:right-[17%] 2xl:right-[20%]">
         <div className="flex-1 lg:mb-0 top-5">
-        <motion.div
-            className="font-bold text-white text-[40px] sm:text-[50px] md:text-[70px] lg:text-[80px] 2xl:text-[90px] leading-[110px] 2xl:leading-[160px] "
+          <motion.div
+            className="font-bold text-white"
             variants={fadeIn("down", "spring", 0.5, 1.5)}
             initial="hidden"
             animate="show"
           >
-            <h1 className="name__ha">Hi, I'm Taejun Ha</h1>
+            <h1 className="name__ha justify-center text-center font-bold text-white text-[40px] sm:text-[40px] md:text-[60px] lg:text-[70px] 2xl:text-[90px] leading-[110px] 2xl:leading-[160px]">
+              Hi, I'm Taejun Ha
+            </h1>
             <div className="roller">
-              <span id="rolltext" className="sm:text-[30px] md:text-[55px] lg:text-[60px] 2xl:text-[60px]">Software developer<br/>
-                Blackhawks fan<br/>
-                Traveller<br/>
-                Runner<br/>
+              <span id="rolltext" className="block sm:text-[30px] md:text-[35px] lg:text-[60px] 2xl:text-[60px]">
+                Software developer<br />
+                Blackhawks fan<br />
+                Traveller<br />
+                Runner<br />
               </span>
-              <br/>
+              <br />
             </div>
-        </motion.div>
+          </motion.div>
         </div>
       </div>
       <img className="parallax_front" src="./parallax/ft.svg" alt="" />
